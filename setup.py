709f1bffe85146b7b633ccea9b77cad3f41da629
Ricardo Juárez <rjuarezp@gmail.com>
2020-12-29 21:12:40 +0100

    New setup.py file

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
-    Executable('main.py', base=base, targetName = 'elaboracion_saldos_v0.2')
+    Executable('main.py', base=base)
 ]
 
-setup(name='Elaboracion_de_saldos',
+setup(name='main',
       version = '0.2',
-      description = 'Elaboracion de Saldos para Ferticampo',
+      description = 'elaboracion de saldos',
       options = {'build_exe': build_options},
       executables = executables)
